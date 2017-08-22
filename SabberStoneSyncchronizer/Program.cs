9c5a162202848b5235f18990bb11e7fd4b07c509
odkken <odkken@gmail.com>
2017-08-22 05:18:31 -0700

    init solution

---
+++ SabberStoneSyncchronizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SabberStoneSyncchronizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
